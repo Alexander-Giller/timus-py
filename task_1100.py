5f59cb9cce7879bb04646164ed5dc6b2c4b82400
Coffee <darkgiller@gmail.com>
2024-12-31 16:22:23 +0300

    Added accepted solution for the last task

--- task_1100.py
+++ task_1100.py
@@ -1,38 +1,16 @@
+
 n: int = int(input())
 
+result = {}
+for i in range(0, 101):
+    result[i] = list()
 
-class TreeNode:
-    def __init__(self, key, value):
-        self.left = None
-        self.right = None
-        self.val = key
-        self.second_val = value
-
-
-def insert(root, key, value):
-    if root is None:
-        return TreeNode(key, value)
-
-    if key <= root.val:
-        root.right = insert(root.right, key, value)
-    else:
-        root.left = insert(root.left, key, value)
-
-    return root
-
-
-def inorder_traversal(root):
-    if root:
-        inorder_traversal(root.left)
-        print(str(root.second_val) + " " + str(root.val))
-        inorder_traversal(root.right)
-
-
-root = None
 for i in range(n):
     input_line = input().split(' ')
     team_id: int = int(input_line[0])
     count: int = int(input_line[1])
-    root = insert(root, count, team_id)
+    result[count].append(team_id)
 
-inorder_traversal(root)
+for task_number, teams_ids in reversed(result.items()):
+    for i in teams_ids:
+        print(str(i) + ' ' + str(task_number) + ' ')
